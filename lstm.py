03cff5d905fdf13bb8bef3206fff589312e3351a
Nies <extnieluc@eniram.fi>
2018-07-13 13:55:37 +0200

    Tried out dec-enc architecture

--- lstm.py
+++ lstm.py
@@ -12,10 +12,10 @@ import os
 seq_length = 100
 
 
-def make_model(input_shape, output_dim):
+def make_model(input_shape, output_dim, batch_size=128):
     print("model dim: ", input_shape, output_dim)
     model = Sequential()
-    model.add(LSTM(256, input_shape=input_shape, return_sequences=True))
+    model.add(LSTM(256, input_shape=input_shape, batch_size=batch_size, return_sequences=True))
     model.add(Dropout(0.2))
     model.add(LSTM(256))
     model.add(Dropout(0.2))
@@ -24,7 +24,8 @@ def make_model(input_shape, output_dim):
     return model
 
 
-def prepare_data(normalize=True):
+def prepare_data():
+    seq_length = 64
     # load ascii text and covert to lowercase
     filename = "wonderland.txt"
     raw_text = open(filename, encoding="utf8").read()
@@ -33,56 +34,55 @@ def prepare_data(normalize=True):
     # create mapping of unique chars to integers
     chars = sorted(list(set(raw_text)))
     char_to_int = dict((c, i) for i, c in enumerate(chars))
+    int_to_char = {number: character for (number, character) in zip(char_to_int.values(), char_to_int.keys())}
 
     n_chars = len(raw_text)
     n_vocab = len(chars)
 
     print("Total Characters: ", n_chars)
-
-    # prepare the dataset of input to output pairs encoded as integers
+    # Convert text to integers and do one-hot encoding
+    int_text = [char_to_int[c] for c in raw_text]
+    int_text = utils.to_categorical(int_text)
     dataX = []
     dataY = []
+
+    # Split sequences
     for i in range(0, n_chars - seq_length, 1):
-        seq_in = raw_text[i:i + seq_length]
-        seq_out = raw_text[i + seq_length]
+        seq_in = int_text[i:i + seq_length]
+        seq_out = int_text[i + seq_length]
 
+        dataX.append(seq_in)
+        dataY.append(seq_out)
 
-        dataX.append([char_to_int[char] for char in seq_in])
-        dataY.append(char_to_int[seq_out])
     n_patterns = len(dataX)
     print("Total Patterns: ", n_patterns)
+    dataX = np.array(dataX)
+    dataY = np.array(dataY)
 
-    dataX = np.reshape(dataX, (n_patterns, seq_length, 1))
-    #if normalize:
-    #   dataX = dataX / float(n_vocab)
-    dataX = utils.to_categorical(dataX)
-    dataY = utils.to_categorical(dataY)
-
-    return raw_text, chars, dataX, dataY
+    return raw_text, chars, dataX, dataY, int_to_char
 
 
-def train():
+def train(batch_size = 512):
 
-    raw_text, chars, dataX, dataY = prepare_data()
+    raw_text, chars, dataX, dataY, int_to_char = prepare_data()
 
     #Make model
     shape = (dataX.shape[1], dataX.shape[2])
-    model = make_model(shape, dataY.shape[1])
+    model = make_model(shape, dataY.shape[1], batch_size=None)
 
     filepath = os.path.join('models', "weights-improvement-{epoch:02d}-bigger.hdf5")
     checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
-
     model.fit(dataX, dataY, epochs=50, batch_size=512, callbacks=callbacks_list)
 
 
-def generate():
+def generate(nth_iter):
 
     # load the network weights
-    raw_text, chars, dataX, dataY = prepare_data(normalize=False)
+    raw_text, chars, dataX, dataY, int_to_char = prepare_data()
     shape = (dataX.shape[1], dataX.shape[2])
     model = make_model(shape, dataY.shape[1])
-    filename = os.path.join('models', "weights-improvement-49-bigger.hdf5")
+    filename = os.path.join('models', "weights-improvement-{}-bigger.hdf5".format(nth_iter))
     model.load_weights(filename)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
     int_to_char = dict((i, c) for i, c in enumerate(chars))
